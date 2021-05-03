a56b2fcd2999bed66cac2b1d6d33700ae1675815
nandOdiaz <fernando_dias99@hotmail.com>
2021-05-03 12:09:23 -0300

    second part of the project

--- src/components/Model/ModelsWrapper/index.tsx
+++ src/components/Model/ModelsWrapper/index.tsx
@@ -1,13 +1,38 @@
 //each whole screen with a car photo, title and buttons
-import React from 'react';
-
+import React, {useRef, useState, useCallback} from 'react';
+import ModelsContext, {CarModel} from '../ModelsContext'
 import { Container } from './styles'
 
 const ModelsWrapper: React.FC = ({children})  => {
+  const wrapperRef = useRef<HTMLDivElement>(null)
+  const [registeredModels, setRegisteredModels] = useState<CarModel[]>([])
+  const registerModel = useCallback((model: CarModel) => {
+    setRegisteredModels(state => [...state, model])
+  }, [])
+
+  const unregisterModel = useCallback((modelName: string) => {
+    setRegisteredModels(state => state.filter(model => model.modelName !== modelName))
+  }, [])
+
+  const getModelByName = useCallback(
+    (modelName: string) => {
+      return registeredModels.find(item => item.modelName === modelName) || null
+    },
+    [registeredModels]
+  )
+
   return (
-    <Container>
-      {children}
-    </Container>
+    <ModelsContext.Provider value={{
+      wrapperRef,
+      registeredModels,
+      registerModel,
+      unregisterModel,
+      getModelByName
+    }}>
+      <Container ref={wrapperRef}>
+        {children}
+      </Container>
+    </ModelsContext.Provider>
   )
 }
 
--- src/components/Model/ModelsWrapper/styles.ts
+++ src/components/Model/ModelsWrapper/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-
+    height: 100vh;
+    scroll-snap-type: y mandatory; // y = top to bottom
+    overflow-y: scroll;
 `;
